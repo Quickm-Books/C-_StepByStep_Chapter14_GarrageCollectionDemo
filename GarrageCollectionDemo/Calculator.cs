73e360a0aaa7d8bd45a5a56119fc62d743587374
quickm <quickm.career@gmail.com>
2023-03-31 23:57:29 -0700

    Create a calculator class with a class destructor

---
+++ GarrageCollectionDemo/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarrageCollectionDemo
+{
+    class Calculator
+    {
+        public Calculator()
+        {
+            Console.WriteLine("Calculator instance created");
+        }
+
+        ~Calculator()
+        {
+            Console.WriteLine("Calculator instance finalized");
+        }
+
+        public int Divide(int first, int second)
+        {
+            return first / second;
+        }
+    }
+}
--- GarrageCollectionDemo/Program.cs
+++ GarrageCollectionDemo/Program.cs
@@ -10,6 +10,9 @@ namespace GarrageCollectionDemo
     {
         static void Main(string[] args)
         {
+            Calculator calculator = new Calculator();
+            Console.WriteLine($"120 / 15 = {calculator.Divide(120, 15)}");
+            Console.WriteLine("Program finishing");
         }
     }
 }
